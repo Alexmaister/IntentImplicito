b08c7bde309790415b21655d4976c5df9a4d3b62
aortiz <aleortzur@gmail.com>
2017-11-20 12:48:55 +0100

    Initial commit

---
+++ app/src/main/java/com/example/aortiz/intentimplicito/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.aortiz.intentimplicito;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+    public void lanzarEmail(View boton){
+
+        EditText texto=(EditText)findViewById(R.id.texto);
+        EditText receptor=(EditText)findViewById(R.id.receptor);
+        EditText asunto=(EditText)findViewById(R.id.asunto);
+
+        Intent lanzarEmail=new Intent(Intent.ACTION_SENDTO);
+        String uri="mailto:"+ Uri.encode(receptor.getText().toString())
+                +"?subject="+Uri.encode(asunto.getText().toString())+
+                "?&body="+Uri.encode(texto.getText().toString());
+        Uri uriV=Uri.parse(uri);
+        lanzarEmail.setData(uriV);
+        startActivity(lanzarEmail);
+    }
+}
